b104f9f3fc0b474ccb23ea413b741300e5224e54
Breeze <78247190+Breeze-Zero@users.noreply.github.com>
2023-09-09 00:34:11 +0800

    Update ESPIRIT_sigpy.py

--- SME/ESPIRIT_sigpy.py
+++ SME/ESPIRIT_sigpy.py
@@ -15,7 +15,7 @@ def ESPIRIT_sigpy(kspace, num_low_freqs,device=0):
 
     # estimate sensitivity maps
     sens_maps = mr.app.EspiritCalib(kspace,calib_width=num_low_freqs,thresh=0.02, kernel_width=6,
-                                    crop=0.9,max_iter=100, device=device,
+                                    crop=0.95,max_iter=100, device=device,
                                     output_eigenvalue=False, show_pbar=False).run().get()
 
     return sens_maps
